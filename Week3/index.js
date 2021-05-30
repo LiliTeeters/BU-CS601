47564ca38c7c779c2da51d668b05c066ee1b0724
Lili Teeters <lilisbet55@gmail.com>
2021-05-30 17:48:16 -0600

    full of prompts

--- Week3/index.js
+++ Week3/index.js
@@ -1,17 +1,16 @@
-
 //function that welcomes, gathers user name, and gives a personalized message to user
-const welcomeScreen = () => {
-    alert('Welcome to my website!')
-    const userName = prompt("What is your name?")
-    alert("Hello " + userName + "! I hope you enjoy your experience.")
+const welcomeScreen =  () => {
+    alert('Welcome to my website!');
+    const userName = prompt("What is your name?");
+    alert("Hello " + userName + "! I hope you enjoy your experience.");
 }
 
 //function that asks the user to enter two numbers, then adds them and displays a message with total
+//this function also loops depending on user selection
 const userNumberInput = () => {
-    
-    var firstNumber = prompt('Please enter a number')
-    var secondNumber = prompt('Please enter a second number')
-    var total = parseInt(firstNumber) + parseInt(secondNumber)
+    var firstNumber = prompt('Please enter a number');
+    var secondNumber = prompt('Please enter a second number');
+    var total = parseInt(firstNumber) + parseInt(secondNumber);
     
     if(total > 10) {
         alert('The sum of your two numbers is:  ' +total +'\n' + 'Wow! That is a big number!');
@@ -19,24 +18,18 @@ const userNumberInput = () => {
         alert('The sum of your two numbers is:  '+total +'\n' + 'That is a small number!');
     } 
     
-    const attempt = prompt('Again: ')
+    const attempt = prompt('Would you like to try another set of numbers? ' +'\n' + 'Please enter "y" or "n"')
     while(attempt) {
-        if(attempt == 'y') {
+        if(attempt == 'Yes') {
             userNumberInput()
         }else{
-            if(attempt == 'n'){
-                alert("Thank you for visiting today!")
-                break;
+            if(attempt == 'No'){
+                alert("Thank you for visiting today!");
+                
             }
-        }
+        }break;
     }
 }
 
 
-
-
-    
-
-welcomeScreen();
 userNumberInput();
-
