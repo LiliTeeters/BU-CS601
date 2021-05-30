23b33bd39e66bdaab96f1d53775ad689000db497
Lili Teeters <lilisbet55@gmail.com>
2021-05-30 14:10:01 -0600

    numbers function

--- Week3/index.js
+++ Week3/index.js
@@ -1,5 +1,4 @@
 
-
 document.write("Hello")
 
 
@@ -8,7 +7,21 @@ const welcomeScreen = () => {
     alert('Welcome to my website!')
     const userName = prompt("What is your name?")
     alert("Hello " + userName + "! I hope you enjoy your experience.")
-
 }
 
-welcomeScreen()
+//function that asks the user to enter two numbers, then adds them and displays a message with total
+const userNumberInput = () => {
+    var firstNumber = prompt('Please enter a number')
+    var secondNumber = prompt('Please enter a second number')
+    var total = parseInt(firstNumber) + parseInt(secondNumber)
+    if(total > 10) {
+        alert('The sum of your two numbers is:  ' +total +'\n' + 'Wow! That is a big number!');
+    } else {
+        alert('The sum of your two numbers is:  '+total +'\n' + 'That is a small number!');
+
+    }
+    
+}
+
+
+userNumberInput();
