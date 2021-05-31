a56c5adfe2bfdee05c5cd72f5cbbaad7bebdeb93
Lili Teeters <lilisbet55@gmail.com>
2021-05-30 20:46:45 -0600

    much improvement

--- Week3/index.js
+++ Week3/index.js
@@ -8,9 +8,10 @@ const welcomeScreen =  () => {
 //function that asks the user to enter two numbers, then adds them and displays a message with total
 //this function also loops depending on user selection
 const userNumberInput = () => {
-    var firstNumber = prompt('Please enter a number');
-    var secondNumber = prompt('Please enter a second number');
+    var firstNumber = prompt('Please enter a whole number');
+    var secondNumber = prompt('Please enter a second whole number');
     var total = parseInt(firstNumber) + parseInt(secondNumber);
+    
     
     if(total > 10) {
         alert('The sum of your two numbers is:  ' +total +'\n' + 'Wow! That is a big number!');
@@ -18,18 +19,17 @@ const userNumberInput = () => {
         alert('The sum of your two numbers is:  '+total +'\n' + 'That is a small number!');
     } 
     
-    const attempt = prompt('Would you like to try another set of numbers? ' +'\n' + 'Please enter "y" or "n"')
+    const attempt = prompt('Would you like to try another set of numbers? ' +'\n' + 'Please enter "yes" or "no"');
     while(attempt) {
-        if(attempt == 'Yes') {
+        if(attempt == 'yes') {
             userNumberInput()
         }else{
-            if(attempt == 'No'){
-                alert("Thank you for visiting today!");
-                
+            if(attempt == 'no'){
+                alert("Thank you for visiting today!");   
             }
         }break;
     }
 }
 
-
-userNumberInput();
+welcomeScreen();
+//userNumberInput();
