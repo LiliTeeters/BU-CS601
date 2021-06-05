7462d9450c142a6a3a9f669348efc5aeb3cb4574
Lili Teeters <lilisbet55@gmail.com>
2021-06-05 15:41:42 -0600

    working submission

--- Week4/index.js
+++ Week4/index.js
@@ -1,33 +1,23 @@
-function validateFirstlName() {
-    var firstName = document.getElementById("firstName")
-    if(firstName.nodeValue.length <=2) {
-        alert("Your name must contain two or more characters.") 
-        firstName.focus()
-        return false
-    }else {
-        return true
-    }
-}
 
-function validateLastName() {
-    var lastName = document.getElementByIdI("lastName")
-    if(lastName.nodeValue.length <=2) {
-        alert("Your last name must contain two or more characters.")
-        lastName.focus()
+function validateForm() {
+    var firstName = document.forms["firstForm"]["firstName"].value;
+    var lastName = document.forms["firstForm"]["lastname"].value;
+    var facilitatorName = document.forms["firstForm"]["facilitator"].value;
+    
+    if ((firstName == '') || (firstName.length <=2)) {
+        alert("Name must be filled out and have two or more characters");
         return false
+
+    }else if ((lastName =='') || (lastName.length <= 2)) {
+        alert("Last Name must be filled out and have two or more characters");
+        return false
+
+    }else if ((facilitatorName == '') || (facilitatorName != "Laura") || (facilitatorName != "Fazil") || (facilitatorName != "Harsh")){
+        alert("Please enter a valid facilitator name")
+        return false
+        
     }else{
         return true
     }
-}
-
-
-
-function validateFacilitator() {
-
-}
-
-
-
-function validateCompletion() {
 
 }
