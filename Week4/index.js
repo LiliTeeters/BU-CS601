6e04276c1e220092f4b4953830c6058602b418de
Lili Teeters <lilisbet55@gmail.com>
2021-06-05 12:31:50 -0600

    adding validation to names

--- Week4/index.js
+++ Week4/index.js
@@ -1,5 +1,23 @@
-function validateName() {
+function validateFirstlName() {
+    var firstName = document.getElementById("firstName")
+    if(firstName.nodeValue.length <=2) {
+        alert("Your name must contain two or more characters.") 
+        firstName.focus()
+        return false
+    }else {
+        return true
+    }
+}
 
+function validateLastName() {
+    var lastName = document.getElementByIdI("lastName")
+    if(lastName.nodeValue.length <=2) {
+        alert("Your last name must contain two or more characters.")
+        lastName.focus()
+        return false
+    }else{
+        return true
+    }
 }
 
 
