5f71f3bce953a6b406ef0c1f18a1fd462d12e93f
Lili Teeters <lilisbet55@gmail.com>
2021-06-06 09:03:37 -0600

    novalidate

--- Week4/index.js
+++ Week4/index.js
@@ -1,24 +1,21 @@
 
-function validateForm(e) {
+function validateForm() {
     const firstName = document.getElementById("firstName")
     const lastName = document.getElementById("lastName")
     const facilitatorName = document.getElementById("facilitator")
     
     
     if (firstName.value.length <=2 || firstName.value == null) {
-        e.preventDefault()
         alert("Name must be filled out and have two or more characters");
         firstName.focus()
         return false
 
     }if (lastName.value.length <= 2 || lastName.value == null) {
-        e.preventDefault()
         alert("Last Name must be filled out and have two or more characters");
         lastName.focus()
         return false
 
     } if ((facilitatorName.value.length <=2) || facilitatorName.value == null || (facilitatorName.value != "Laura") || (facilitatorName.value != "Fazil") || (facilitatorName.value != "Harsh")){
-        e.preventDefault()
         alert("Please enter a valid facilitator name");
         facilitatorName.focus()
         return false
