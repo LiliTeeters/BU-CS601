1b58db7dad19247985318a9163d9f77a3a30e1fd
Lili Teeters <lilisbet55@gmail.com>
2021-06-07 20:29:43 -0600

    final

--- Week4/index.js
+++ Week4/index.js
@@ -1,26 +1,34 @@
 
 function validateForm() {
-    const firstName = document.getElementById("firstName")
-    const lastName = document.getElementById("lastName")
-    const facilitatorName = document.getElementById("facilitator")
-    
-    
-    if (firstName.value.length <=2 || firstName.value == null) {
+    const firstName = document.getElementById("firstName").value
+    const lastName = document.getElementById("lastName").value
+    const facilitatorName = document.getElementById("facilitator").value
+
+    if (firstName.length <=2 || firstName == null) {
         alert("Name must be filled out and have two or more characters");
         firstName.focus()
         return false
 
-    }if (lastName.value.length <= 2 || lastName.value == null) {
+    }
+     else if (lastName.length <= 2 || lastName == null) {
         alert("Last Name must be filled out and have two or more characters");
         lastName.focus()
         return false
 
-    } if ((facilitatorName.value.length <=2) || facilitatorName.value == null || (facilitatorName.value != "Laura") || (facilitatorName.value != "Fazil") || (facilitatorName.value != "Harsh")){
-        alert("Please enter a valid facilitator name");
+    }
+     else if (facilitatorName.length <=2 || facilitatorName == null) {
+        alert("Facilitator name must be filled out and have two or more characters");
         facilitatorName.focus()
         return false
-
+     }
+     else if (facilitatorName.value != "Fazil" || facilitatorName.value != "Laura" || facilitatorName.value != "Harsh"){
+         alert("Please enter a valid facilitator name")
+         facilitatorName.focus()
+         return false    
+         
+     } else {
+     
+     return true
+     }
+     
     }
-    return true
-
-}
