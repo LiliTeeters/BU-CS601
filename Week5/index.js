2f4b585b06a4ff02a9acf90d0b714c7d74aae821
Lili Teeters <lilisbet55@gmail.com>
2021-06-13 21:27:23 -0600

    functions

--- Week5/index.js
+++ Week5/index.js
@@ -1,1 +1,32 @@
 
+var urlToEducation = "https://api.jsonbin.io/b/60c6aa8fb274176a77e71749"
+
+(function () {
+    var httpRequest;
+    document.getElementById("button").onclick = function() {
+        makeRequest("education.html");
+    }
+
+
+function makeRequest(urlToEducation) {
+    httpRequest = new XMLHttpRequest();
+    if (!httpRequest) {
+        alert("error")
+        return false;
+    }
+
+    httpRequest.onreadystatechange = alertContents;
+    httpRequest.open('GET', urlToEducation)
+    httpRequest.send()
+}
+
+function alertContents() {
+    if (httpRequest.readyState === XMLHttpRequest.DONE) {
+        if (httpRequest.status === 200) {
+            alert(httpRequest.responseText)
+        } else {
+            alert('problem')
+        }
+    }
+}
+})()
